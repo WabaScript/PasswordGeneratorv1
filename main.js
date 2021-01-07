fbf42f36fe43dbee3a36e0d6bdb7e6417f9124f0
wabascript <rjwahba@gmail.com>
2021-01-06 19:32:35 -0500

    clipboard

--- main.js
+++ main.js
@@ -32,6 +32,22 @@ generateEl.addEventListener('click', () => {
     );
 });
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert("Password copied to clipoard!")
+})
+
 // Generate Password Function
 function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = '';
