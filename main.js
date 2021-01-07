93a556d1cb06eee844b964f09c3c361faa23dd8e
wabascript <rjwahba@gmail.com>
2021-01-06 19:00:38 -0500

    logic

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-// Generator Functions
+// DOM Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+// Actions
+generateEl.addEventListener('click', () => {
+    const length = parseInt(lengthEl.value);
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
+    );
+});
+
+// Generate Password Function
+function generatePassword(lower, upper, number, symbol, length) {
+    
+}
+
+// Generator Functions
 // lowercase = 97 - 122
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
